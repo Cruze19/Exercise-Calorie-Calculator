d5b9d160e6b158b9a0736959f6f9b3804efc6027
Cruze19 <enisc787@gmail.com>
2025-04-05 21:30:10 -0400

    Initial Commit

---
+++ Exercise Calorie Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise Calorie Calculator
+//
+//  Created by Jamisel Roman on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
